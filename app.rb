04799b50db8a68ac498d0347e178170d785870f1
Oskar Jansson <oskja067@student.liu.se>
2017-09-09 16:16:10 +0200

    adds: '/login' redirect

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ get '/' do
   erb :index
 end
 
+get '/login' do
+  redirect '/dashboard'
+end
+
 get '/dashboard' do
   url = "http://vitalbond.azurewebsites.net/home/sql?0=Esher"
   payload = "iGetSensorData 2, 'Humidity', '20170909 12:56', '20170909 13:03'"
